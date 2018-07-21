7108ae1288e3de2536e05e88e7129235a7955966
Stuart Grimshaw <stuart.grimshaw@gmail.com>
2018-07-21 16:04:45 +0100

    Integrated routines from FastLED to handle RGB LED

--- bin_day.ino
+++ bin_day.ino
@@ -1,25 +1,42 @@
-#include <dummy.h>
-
 #include "HTTPSRedirect.h"
 #include <SPI.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <FastLED.h>
 
+const char* ssid = "TP-LINK_5A6B";
+const char* password = "43396153";
+const char* calendar_id = "AKfycbxdxppvk0O9S7zMECsDGRG2gpjgjt74SJt4UFd5jgZHIYr1bJk";
 
-#define NBR_EVENTS 3
-//#define UPDATETIME 3600000
-#define UPDATETIME 30000
+#define NBR_EVENTS 4
+#define UPDATETIME 3600000
+//#define UPDATETIME 30000
 
 #define BLACKBIN 0
 #define BLUEBIN 1
 
+#define YELLOW 2
+#define RED 3
+
 #define PULSE_DELAY 15
+
+#define OTA_NODE_NAME "BIN_01"
+#define OTA_PASSWORD "woowb2018"
+
 
 const int REDPIN   = D2;
 const int GREENPIN = D3;
 const int BLUEPIN  = D4;
 
-String possibleEvents[NBR_EVENTS] = {"black", "blue", "yellow"};
-byte led_colour[NBR_EVENTS][3] = {{255, 255, 255}, {0, 0, 255}, {255, 255, 0}};
+String possibleEvents[NBR_EVENTS] = {"black", "blue", "yellow", "red"};
+CRGB led_colour[NBR_EVENTS] = {
+  CRGB::White, 
+  CRGB::Blue,
+  CRGB::Yellow,
+  CRGB::Red
+ };
 byte led_off[3] = {0, 0, 0};
 
 //Connection Settings
@@ -35,10 +52,10 @@ int binDay = -1;
 
 HTTPSRedirect* client = nullptr;
 
-void setColour(byte colour[3]) {
-  analogWrite(REDPIN, colour[0]);
-  analogWrite(GREENPIN, colour[1]);
-  analogWrite(BLUEPIN, colour[2]);
+void setColour(const CRGB& colour) {
+  analogWrite(REDPIN, colour.r);
+  analogWrite(GREENPIN, colour.g);
+  analogWrite(BLUEPIN, colour.b);
 }
 
 void connectToWiFi() {
@@ -57,6 +74,8 @@ void connectToWiFi() {
 String getCalendar() {
   String calendarData = "";
   
+  setColour(CRGB::Red);
+  
   Serial.println("Start Request for calendar.");
 
   client = new HTTPSRedirect(httpsPort);
@@ -65,12 +84,12 @@ String getCalendar() {
 
   if (!client->connected()) client->connect(host, httpsPort);
 
-  if(client->GET(url, host)) {
+  if (client->GET(url, host)) {
     calendarData = client->getResponseBody();
 
     Serial.print("Calender Data: ");
     Serial.println(calendarData);
-  
+
     attempts++;
   } else {
     String reason = client->getStatusCode() + ": " + client->getReasonPhrase();
@@ -83,7 +102,7 @@ String getCalendar() {
 }
 
 void manageStatus(String calendarData) {
-  setColour(led_off);
+  setColour(CRGB::Black);
 
   for (int i = 0; i < NBR_EVENTS; i++) {
     Serial.println("Checking for " + possibleEvents[i] + " bin day");
@@ -95,56 +114,67 @@ void manageStatus(String calendarData) {
 }
 
 void fadeWhite() {
-  byte currentColour[3] = {255, 255, 255};
-  for (int i = 255; i >= 50; i--) {
-    currentColour[0] = i;
-    currentColour[1] = i;
-    currentColour[2] = i;
-    setColour(currentColour);
-    delay(PULSE_DELAY);
-  }
-
-  for (int i = 50; i <= 255; i++) {
-    currentColour[0] = i;
-    currentColour[1] = i;
-    currentColour[2] = i;
-    setColour(currentColour);
+  for (int i = 512; i >= 0; i--) {
+    setColour( CHSV(255, 0, sin8(i)));
     delay(PULSE_DELAY);
   }
 }
 
 void fadeBlue() {
-  byte currentColour[3] = {0, 0, 255};
-  for (int i = 255; i >= 50; i--) {
-    currentColour[2] = i;
-    setColour(currentColour);
-    delay(PULSE_DELAY);
-  }
-
-  for (int i = 50; i <= 255; i++) {
-    currentColour[2] = i;
-    setColour(currentColour);
+  for (int i = 512; i >= 0; i--) {
+    setColour( CHSV(160, 255, sin8(i)) );
     delay(PULSE_DELAY);
   }
 }
 
 void setup() {
-  Serial.begin(9600);
-
-  setColour(led_colour[2]);
-
-  Serial.println();
-
-  connectToWiFi();
+  Serial.begin(115200);
 
   pinMode(REDPIN, OUTPUT);
   pinMode(GREENPIN, OUTPUT);
   pinMode(BLUEPIN, OUTPUT);
 
+  setColour(CRGB::Yellow);
+
+  Serial.println();
+
+  connectToWiFi();
+
+  ArduinoOTA.setHostname(OTA_NODE_NAME);
+  ArduinoOTA.setPassword((const char *)(OTA_PASSWORD));
+  
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+
   delay(5000);
 }
 
 void loop() {
+  ArduinoOTA.handle();
+  
   if (entryUpdated == 0 || millis() > entryUpdated + UPDATETIME) {
     String calendarData = getCalendar();
     manageStatus(calendarData);
@@ -159,7 +189,7 @@ void loop() {
     case BLUEBIN:
       fadeBlue();
       break;
-    
+
   }
 
   delay(10);
